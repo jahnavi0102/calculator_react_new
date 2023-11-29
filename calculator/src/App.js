4bee53208b8595e8fd89fe2aa3cb1c15a407b945
jahnavi0102 <jjain.rjc@gmail.com>
2023-11-29 13:59:25 +0530

    css style borders

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,29 +3,31 @@ import './index.css';
 function App() {
   return (
     <div className='flex items-center justify-center h-screen'>
-      <div className='shadow-2xl w-64 h-96 rounded-lg bg-neutral-900 '>
-        <div className='h-24 w-50 border-rose-500 bg-neutral-600 rounded-lg'>
+      <div className='w-64 h-96 rounded-lg '>
+        <div className='h-24 w-50 border-double border-4 border-x-pink border-t-pink bg-black rounded-lg '>
           <div className='previous-operand'></div>
           <div className='current-operand'></div>
         </div>
-        <button className='span-two'>AC</button>
-        <button>DEL</button>
-        <button>/</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className='span-two'>=</button>
+        <div className=' h-72 w-50 border-double border-4 border-pink bg-black rounded-lg'>
+          <button className='span-two'>AC</button>
+          <button>DEL</button>
+          <button>/</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>*</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button className='span-two'>=</button>
+        </div>
       </div>
       </div>
   );
--- calculator/tailwind.config.js
+++ calculator/tailwind.config.js
@@ -4,7 +4,60 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'turquoise':'#05d9e8',
+      'semi-dark-blue': '#005678',
+      'dark-blue': '#01012b',
+      'red': '#d1f7ff',
+      'pink': '#ff184c',
+      'light-pink':'#ffccdc',
+      'purple-grey': '#575267',
+      'green': '#65dc98',
+      'light-green': '#a0ffe3',
+      'yellow': '#defe47',
+      'pale=orage': '#e96d5e',
+      'dark-green': '#083e12',
+      'light-neon-green': '#1afe49',
+      'black': '#000000'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      borderColor: {
+        custom: {
+          'turquoise':'#05d9e8',
+          'semi-dark-blue': '#005678',
+          'dark-blue': '#01012b',
+          'red': '#d1f7ff',
+          'pink': '#ff184c',
+          'light-pink':'#ffccdc',
+          'purple-grey': '#575267',
+          'green': '#65dc98',
+          'light-green': '#a0ffe3',
+          'yellow': '#defe47',
+          'pale=orage': '#e96d5e',
+          'dark-green': '#083e12',
+          'light-neon-green': '#1afe49',
+          'black': '#000000'
+        },
+      },
+
+    }
   },
   plugins: [],
 }
